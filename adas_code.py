8567e2e82cd15346f48fc870fc508f70b4931d11
gboughton <ga02boug@siena.edu>
2015-12-08 15:43:29 -0500

    misc changes

--- adas_code.py
+++ adas_code.py
@@ -1,5 +1,5 @@
 import numpy as np
 
 def read_data(file):
-    filename = np.loadtxt('(file)',delimiter=',',skiprows=0,unpack=True)
-    return filename
+    filename = np.loadtxt(file,delimiter=',',unpack=True)
+    return filename[1]
